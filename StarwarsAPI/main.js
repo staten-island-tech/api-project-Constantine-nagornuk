07bec7e07ce298474916d4ee5887f335d14bfc17
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-12-01 13:15:32 -0500

    NOT WOKING

--- StarwarsAPI/main.js
+++ StarwarsAPI/main.js
@@ -1,24 +1,30 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const url = "https://swapi.dev/api/people"
 
-setupCounter(document.querySelector('#counter'))
+async function getdata(url) {
+
+  try {
+    const repsonce = await fetch(url)
+    console.log(repsonce)
+    if (repsonce.status != 200) {
+      throw new Error(repsonce.statusText)
+    }
+    const data = await repsonce.json()
+    
+
+  }
+  catch (error){
+    document.querySelector("h1").textContent = error
+
+  }
+
+   
+  
+}
+
+getdata(url)
+
+
+
+console.log("HHEHEHHE")
