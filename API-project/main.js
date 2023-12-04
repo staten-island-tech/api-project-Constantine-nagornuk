4d090fbc227059e27d1d2b1488c332dde687a937
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-12-04 13:09:24 -0500

    test

---
+++ API-project/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+ const url = "https://swapi.dev/api/people"
+  async function getdata(url) 
+  { try { const repsonce = await fetch(url) 
+    console.log(repsonce) 
+    if (repsonce.status != 200) 
+    { throw new Error(repsonce.statusText) } 
+    const data = await repsonce.json() } 
+    catch (error){ document.querySelector("h1").textContent = error } } 
+
+    getdata(url)
